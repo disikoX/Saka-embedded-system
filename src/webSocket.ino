995f9a1a68d5f8f292eb09b1dee2ec694cfc40a3
disikoX <damien.zara12@gmail.com>
2025-05-22 23:40:40 +0300

    fix: typo error on `handleWebSocketMessage`

--- src/webSocket.ino
+++ src/webSocket.ino
@@ -13,7 +13,7 @@ AsyncWebSocket ws("/ws");
 
 
 
-funvoid handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
