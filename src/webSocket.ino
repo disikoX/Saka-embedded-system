a382fbd06a424242e788e5ac9b6bab7039329f33
disikoX <damien.zara12@gmail.com>
2025-05-22 20:52:56 +0300

    fix: corrected the handler

--- src/webSocket.ino
+++ src/webSocket.ino
@@ -17,8 +17,6 @@ funvoid handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
-    if (strcmp((char*)data, "toggle") == 0) {
-      ledState = !ledState;
       notifyClients();
     }
   }
