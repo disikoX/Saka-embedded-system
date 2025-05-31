fa6bdce2e9004ceb2aedbb7fc0dcf036f385bb4d
disikoX <damien.zara12@gmail.com>
2025-05-31 23:04:11 +0300

    chores(refactor): get data from `firebase`

--- src/actions.ino
+++ src/actions.ino
@@ -9,8 +9,19 @@
 
 #define LED_PIN 2  // GPIO pin for LED
 
-// Define Firebase Data object
-FirebaseData firebaseData;
+
+
+SSL_CLIENT ssl_client;
+
+using AsyncClient = AsyncClientClass;
+AsyncClient aClient(ssl_client);
+
+UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD, 3000 /* expire period in seconds (<3600) */);
+FirebaseApp app;
+RealtimeDatabase Database;
+AsyncResult databaseResult;
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -32,20 +43,64 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 
-  // Set up streaming for LED status changes
-  if (!Firebase.beginStream(firebaseData, "/TriggerNow")) {
-    Serial.println("Could not begin stream");
-    Serial.println("REASON: " + firebaseData.errorReason());
-  }
+
+  set_ssl_client_insecure_and_buffer(ssl_client);
+
+  Serial.println("Initializing app...");
+  initializeApp(aClient, app, getAuth(user_auth), auth_debug_print, "🔐 authTask");
+
+  app.getApp<RealtimeDatabase>(Database);
+  Database.url(DATABASE_URL);
 }
 
 void loop() {
-  if (Firebase.available(firebaseData)) {
-    if (firebaseData.dataType() == "boolean") {
-      bool ledState = firebaseData.boolData();
-      digitalWrite(LED_PIN, ledState ? HIGH : LOW);
-      Serial.println("LED state changed to: " + String(ledState ? "ON" : "OFF"));
+
+  app.loop();
+
+  if(app.ready()){
+    
+    String value = Database.get(aClient, "/users/{usersId}/distributors/triggerNow");
+    
+    if(value == "true") {
+    DigitalWrite(LED_PIN,HIGH);
+    }else {
+      DigitalWrite(LED_PIN,LOW);
     }
+
   }
-  delay(100);
+
 }
+
+template <typename T>
+void check_and_print_value(T value){
+  // To make sure that we actually get the result or error.
+  if (aClient.lastError().code() == 0){
+    Serial.print("Success, Value: ");
+    Serial.println(value);
+  }
+  else {
+    Firebase.printf("Error, msg: %s, code: %d\n", aClient.lastError().message().c_str(), aClient.lastError().code());
+  }
+}
+
+void processData(AsyncResult &aResult){
+  if (!aResult.isResult())
+    return;
+
+  if (aResult.isEvent())
+    Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.eventLog().message().c_str(), aResult.eventLog().code());
+
+  if (aResult.isDebug())
+    Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+
+  if (aResult.isError())
+    Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+
+  if (aResult.available()){
+    // Log the task and payload
+    Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
+  }
+}
+
+
+
