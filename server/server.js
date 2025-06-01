41e4b2eaf8f3272cb31a9f10d0cca8474e9d9644
josoa-ramaroson <ramaroson.josoa.r@gmail.com>
2025-06-01 18:42:29 +0300

    websocket and some craft from actions

--- server/server.js
+++ server/server.js
@@ -3,15 +3,25 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const admin = require("firebase-admin");
-
+const axios = require("axios");
+const expressWs = require('express-ws');
 const app = express();
+
+const WebSocket = require('ws');
 
 // Middlewares
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+expressWs(app);
 
+app.ws('/', (ws, req) => {
+  ws.on('message', (msg) => {
+    console.log(`Message reçu : ${msg}`);
+    ws.send(`Echo : ${msg}`);
+  });
+});
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require(process.env.FIREBASE_CONFIG);
@@ -21,16 +31,53 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-const ref = db.ref('/users/{usersId}/distributors/triggerNow');
+const ref = db.ref('users');
+// const ref = db.ref('/users/{usersId}/distributors/{distributorId}/triggerNow');
 
 // API endpoint to toggle LED
-app.post('/toggle-led', (req, res) => {
-  const { status } = req.body;
-  ref.set(status)
-    .then(() => res.send({ success: true }))
-    .catch(err => res.status(500).send({ success: false, error: err }));
+app.post('/on-led', async (req, res) => {
+  const { led } = req.body;
+  try {
+    await axios.get(`http://${process.env.ESP32_IP}/${led}/on`);
+    res.send({ success: true });
+  } catch (error) {
+    console.error('Error turning on LED:', error);
+    res.status(500).send({ success: false, error: error.message });
+  }
 });
 
+app.post('/off-led', async (req, res) => {
+  const { led } = req.body;
+  try {
+    await axios.get(`http://${process.env.ESP32_IP}/${led}/off`);
+    res.send({ success: true });
+  } catch (error) {
+    console.error('Error turning off LED:', error);
+    res.status(500).send({ success: false, error: error.message });
+  }
+});
+
+app.get("/:distributorId/userId", async (req, res) => {
+  try {
+    const { distributorId } = req.params;
+    console.log("Fetching data for distributorId:", distributorId);
+    const snapshot = await db
+                        .ref(`/distributors/${distributorId}`)
+                        .child('assignedTo')
+                        .once('value');
+                        
+    const data = snapshot.val();
+    if (!data) {
+      return res.status(404).send({ error: 'No data found' });
+    }
+
+    res.send(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send({ error: 'Failed to fetch data' });
+  }
+}
+);
 // Get current LED status
 app.get('/led-status', (req, res) => {
   ref.once('value')
